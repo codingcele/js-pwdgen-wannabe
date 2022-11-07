ea83f83080bae0cc4cf20232c3b4ed8beaa93cd1
codingcele <marcocelegato9@gmail.com>
2022-11-07 19:21:13 +0100

    finito

---
+++ main.js
@@ -0,0 +1,10 @@
+
+let nome = prompt("What's your name?");
+
+let surname = prompt("What's your last name?");
+
+let favColor = prompt("What's your favourite color?");
+
+let age = prompt("How old are you?");
+
+document.getElementById("getPassword").innerHTML = `Your password is: ` + nome + surname + favColor + age;
